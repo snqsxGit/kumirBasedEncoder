5ad90450da3d3cf3515fa07223974194d227ae63
snqsxGit <codeforcenoob@gmail.com>
2025-02-24 16:47:39 +0400

    Update app.py

--- app.py
+++ app.py
@@ -71,6 +71,7 @@ def index():
     return render_template("index.html")
 
 if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))  # Render передаёт PORT, локально — 5000
+    app.run(host="0.0.0.0", port=port, debug=True)
     os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
     os.makedirs(app.config['OUTPUT_FOLDER'], exist_ok=True)
-    app.run(debug=True)
